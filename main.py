2712a690b0bdadc89bada0cf9ea5f127dc11dbe2
keshe4ka <a.anaschenko@btc-s.ru>
2023-10-23 00:49:41 +0300

    feat: change get com method

--- main.py
+++ main.py
@@ -6,24 +6,11 @@ import serial.tools.list_ports
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # BUTTON
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # BUTTON
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)  # RED LIGHT
 GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)  # GREEN LIGHT
 
 ESP_32_DESC = 'CP2102 USB to UART Bridge Controller'
-ESP_32_COM = None
-
-
-def is_com_set() -> bool:
-    global ESP_32_COM
-
-    for p in [tuple(p) for p in serial.tools.list_ports.comports()]:
-        if ESP_32_DESC in p[1]:
-            ESP_32_COM = p[0]
-
-    if not ESP_32_COM:
-        return False
-    return True
 
 
 def red_light(enable: bool):
@@ -40,22 +27,45 @@ def green_light(enable: bool):
         GPIO.output(7, GPIO.LOW)
 
 
+def get_com_port() -> str | None:
+    for p in [tuple(p) for p in serial.tools.list_ports.comports()]:
+        if ESP_32_DESC in p[1]:
+            return p[0]
+    return None
+
+
 def main():
-    while True:
-        red_light(True)
-        # if button pressed
-        if GPIO.input(3) == GPIO.HIGH:
-            if not is_com_set():
-                print('Device is not connected')
-                return False
-            red_light(True)
-            green_light(True)
-            esptool.main(['-p', ESP_32_COM, 'flash_id'])
+    red_light(True)
+    # if button pressed
+    if GPIO.input(10) == GPIO.HIGH:
+
+        # get com_port
+        com_port = get_com_port()
+        if not com_port:
+            print('Device is not connected')
+
+            # show LEDS for device is not connected
             red_light(False)
-            time.sleep(5)
-            green_light(False)
-        time.sleep(0.2)
+            time.sleep(0.1)
+            red_light(True)
+            time.sleep(0.1)
+            red_light(False)
+            time.sleep(0.1)
+            red_light(True)
+
+            return False
+
+        # show LEDS for firmware upload
+        red_light(True)
+        green_light(True)
+
+        # upload firmware with esptool
+        esptool.main(['-p', com_port, 'flash_id'])
+
+        # show LEDS for successfully
         red_light(False)
+        time.sleep(5)
+        green_light(False)
 
 
 if __name__ == '__main__':
